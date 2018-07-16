dc2c8e7e6b22e0f4960bac011fb9f9ba00824872
Tiffany Trujillo <tiffanytrujillo@Tiffanys-MacBook-Air.local>
2018-07-16 09:36:12 -0400

    Created paragraphs variable and altered style

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ $(document).ready(function(){
 });
 
 $('h2').append(" I'm Tiffany Trujillo, and I'm learning jQuery!")
+
+$(document).ready(function(){
+  var paragraphs = $('p');
+  paragraphs.css({
+    'font-size': '25px',
+    'color': 'pink',
+    'padding': '1%'
+  });
+});
